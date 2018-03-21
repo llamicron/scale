ea7a13378ac4e6141ad545f1120d85894e4249e8
Luke Sweeney <luke@thesweeneys.org>
2018-03-20 22:00:52 -0500

    Buncha stuff, no time to write good commit messages lol

--- scales.js
+++ scales.js
@@ -1,56 +1,66 @@
+scales = [
+  {
+    elementId: "c-lydian",
+    tonic: "C",
+    mode: "Lydian",
+    notes: ["C/4", "D/4", "E/4", "F#/4", "G/4", "A/4", "B/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-ionian",
+    tonic: "C",
+    mode: "Ionian",
+    notes: ["C/4", "D/4", "E/4", "F/4", "G/4", "A/4", "B/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-mixolydian",
+    tonic: "C",
+    mode: "Mixolydian",
+    notes: ["C/4", "D/4", "E/4", "F/4", "G/4", "A/4", "Bb/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-dorian",
+    tonic: "C",
+    mode: "Dorian",
+    notes: ["C/4", "D/4", "Eb/4", "F/4", "G/4", "A/4", "Bb/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-aeolian",
+    tonic: "C",
+    mode: "Aeolian",
+    notes: ["C/4", "D/4", "Eb/4", "F/4", "G/4", "Ab/4", "Bb/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-phrygian",
+    tonic: "C",
+    mode: "Phrygian",
+    notes: ["C/4", "Db/4", "Eb/4", "F/4", "G/4", "Ab/4", "Bb/4", "C/5"],
+    tags: []
+  },
+  {
+    elementId: "c-locrian",
+    tonic: "C",
+    mode: "Locrian",
+    notes: ["C/4", "Db/4", "Eb/4", "F/4", "Gb/4", "Ab/4", "Bb/4", "C/5"],
+    tags: []
+  },
+]
+
 VF = Vex.Flow;
 
-var scales = new Vue({
+var scale = new Vue({
   el: "#main",
   data: {
-    scales: [
-      {
-        elementId: "c-lydian",
-        tonic: "C",
-        mode: "Lydian",
-        notes: ["C/4", "D/4", "E/4", "F#/4", "G/4", "A/4", "B/4", "C/5"]
-      },
-      {
-        elementId: "c-ionian",
-        tonic: "C",
-        mode: "Ionian",
-        notes: ["C/4", "D/4", "E/4", "F/4", "G/4", "A/4", "B/4", "C/5"]
-      },
-      {
-        elementId: "c-mixolydian",
-        tonic: "C",
-        mode: "Mixolydian",
-        notes: ["C/4", "D/4", "E/4", "F/4", "G/4", "A/4", "Bb/4", "C/5"]
-      },
-      {
-        elementId: "c-dorian",
-        tonic: "C",
-        mode: "Dorian",
-        notes: ["C/4", "D/4", "Eb/4", "F/4", "G/4", "A/4", "Bb/4", "C/5"]
-      },
-      {
-        elementId: "c-aeolian",
-        tonic: "C",
-        mode: "Aeolian",
-        notes: ["C/4", "D/4", "Eb/4", "F/4", "G/4", "Ab/4", "Bb/4", "C/5"]
-      },
-      {
-        elementId: "c-phrygian",
-        tonic: "C",
-        mode: "Phrygian",
-        notes: ["C/4", "Db/4", "Eb/4", "F/4", "G/4", "Ab/4", "Bb/4", "C/5"]
-      },
-      {
-        elementId: "c-locrian",
-        tonic: "C",
-        mode: "Locrian",
-        notes: ["C/4", "Db/4", "Eb/4", "F/4", "Gb/4", "Ab/4", "Bb/4", "C/5"]
-      },
-    ]
+    scales: scales,
+    filterInput: ""
   },
 
   methods: {
-    drawStaves: function() {
+    drawStaves() {
       this.scales.forEach(scale => {
         var div = document.getElementById(scale.elementId);
         var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
@@ -68,7 +78,6 @@ var scales = new Vue({
           if (accidental == '/') {
             accidental = false;
           }
-          console.log(note)
           newNote = new VF.StaveNote({ clef: "treble", keys: [note], duration: "q" });
           if (accidental) {
             newNote.addAccidental(0, new VF.Accidental(accidental));
@@ -88,10 +97,34 @@ var scales = new Vue({
 
         stave.setContext(context).draw();
       });
+    },
+
+    generateTags() {
+      // Auto tags are: tonic, mode
+      for (let index = 0; index < this.scales.length; index++) {
+        const scale = this.scales[index];
+        scale.tags = [];
+        scale.tags.push(scale.tonic);
+        if (scale.mode == "Aeolian") {
+          scale.tags.push("Minor");
+        } else if (scale.mode == "Ionian") {
+          scale.tags.push("Major");
+        }
+        scale.tags.push(scale.mode);
+      }
+    },
+
+    addFilter(tag) {
+      if (this.filterInput.includes(tag)) {
+        return false;
+      }
+      this.filterInput = this.filterInput.trim();
+      this.filterInput += " " + tag.trim();
     }
   },
 
   mounted() {
+    this.generateTags();
     this.drawStaves();
   }
 })
